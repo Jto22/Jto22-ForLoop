c5b6e174909232e0a915c448c5ff09367dafc4dd
Jto22 <brendonfils@gmail.com>
2023-02-23 10:32:01 +0000

    For Loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -43,7 +43,8 @@ public class Lab {
         String buildingString = "";
 
 //        code here
-
-        return buildingString;
+        for(int i=start; i<end; i++ ) {
+            buildingString = buildingString + i +" ";
+        }   return buildingString;
     }
 }
